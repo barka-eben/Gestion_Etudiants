22ab54a80073a9ded6d51a1f74c9ce7937d0f420
BARKA EBEN-EZEER <barkaebenezeer@gmail.com>
2026-01-07 08:42:20 +0100

    Initialisation des fichiers

--- bibliotheque.h
+++ bibliotheque.h
@@ -1,53 +1,5 @@
-#ifndef ETUDIANT_H
-#define ETUDIANT_H
+#ifndef BIBLIOTHEQUE_H
+#define BIBLIOTHEQUE_H
 
-#define MAX_ETUDIANTS 100
-#define MAX 10
-#define ANNEE 2026
-
-/* Structure Etudiant */
-typedef struct {
-    char matricule[20];
-    char nom[30];
-    char prenom[30];
-    char date_naissance[15];
-    char sexe[10];
-    char departement[30];
-    char filiere[30];
-    char region_origine[30];
-} Etudiant;
-
-/* Prototypes des fonctions */
-void ajouterEtudiant(Etudiant etudiants[], int *n);
-int rechercherEtudiant(Etudiant etudiants[], int n, char matricule[]);
-void modifierEtudiant(Etudiant etudiants[], int n);
-
-// enregistrement date
-typedef struct
-{
-    int jour;
-    int mois;
-    int annee;
-} date;
-// enregistrement etudiant
-typedef struct
-{
-    char matricule[50];
-    char nom[50];
-    char prenom[100];
-    char departement[50];
-    char option[50];
-    char sexe[2];
-    char region[10];
-    date naissance;
-} etudiant;
-
-/* presentation de mes fonctions */
-int ajouter(etudiant tab[], int n);
-void rechercher(etudiant tab[], int n);
-void modifier(etudiant tab[], int n);
-int supprimer(etudiant tab[], int n);
-void trienom(etudiant tab[], int n);
-void afficher(etudiant tab[], int n);
 
 #endif
--- main.c
+++ main.c
@@ -1,34 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include "bibliotheque.h"
-#include "etudiant.h"
-
-int main() {
-    Etudiant etudiants[MAX_ETUDIANTS];
-    int n = 0;
-    int choix;
-
-    do {
-        printf("\n--- MENU ---\n");
-        printf("1. Ajouter un etudiant\n");
-        printf("2. Modifier un etudiant\n");
-        printf("0. Quitter\n");
-        printf("Choix : ");
-        scanf("%d", &choix);
-
-        switch (choix) {
-            case 1:
-                ajouterEtudiant(etudiants, &n);
-                break;
-            case 2:
-                modifierEtudiant(etudiants, n);
-                break;
-            case 0:
-                printf("Fin du programme.\n");
-                break;
-            default:
-                printf("Choix invalide.\n");
-        }
-    } while (choix != 0);
-
+int main(){
+    
     return 0;
 }
