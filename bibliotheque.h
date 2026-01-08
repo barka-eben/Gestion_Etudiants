0619d43a7057d28786ef6217dad69f9a3ac367bf
BARKA EBEN-EZEER <barkaebenezeer@gmail.com>
2026-01-08 05:06:42 +0100

    Initialisation des fichiers

--- bibliotheque.h
+++ bibliotheque.h
@@ -1,30 +1,5 @@
 #ifndef BIBLIOTHEQUE_H
 #define BIBLIOTHEQUE_H
 
-typedef struct {
-    int jour;
-    int mois;
-    int annee;
-} Date;
-
-typedef struct {
-    char matricule[11];
-    char nom[30];
-    char prenom[30];
-    Date date_naissance;
-    char departement[30];
-    char filiere[50];
-    char sexe;
-    char region_origine[50];
-} Etudiant;
-
-void ajout_etudiant();
-void afficher_etudiants();
-void supprimer_etudiant();
-void modifier_etudiant();
-void rechercher_etudiant();
-void tri_alphabetique();
-void tri_par_filiere();
-void recherche_matricule_dichotomie();
 
 #endif
