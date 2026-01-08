d85a4d5f0858f009c8b76e67c2f14c5fd3f1385b
BARKA EBEN-EZEER <barkaebenezeer@gmail.com>
2026-01-08 05:45:21 +0100

    Déclaration des structures

--- bibliotheque.h
+++ bibliotheque.h
@@ -1,5 +1,21 @@
 #ifndef BIBLIOTHEQUE_H
 #define BIBLIOTHEQUE_H
 
+typedef struct {
+    int jour;
+    int mois;
+    int annee;
+} Date;
+
+typedef struct {
+    char matricule[11];
+    char nom[30];
+    char prenom[30];
+    Date date_naissance;
+    char departement[30];
+    char filiere[50];
+    char sexe;
+    char region_origine[50];
+} Etudiant;
 
 #endif
